ba91e0bbc8d1ef9b423aa26298e7228c867b4a12
Vadim Storozhuk <vadozy@yahoo.com>
2019-05-27 23:19:35 -0400

    Fitst commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './BasicRouter/AppRouter';
+import App from './NestedRouter/AppRouter';
+
+ReactDOM.render(<App />, document.getElementById('root'));
